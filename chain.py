a44002bf48358174b21cf80256abf47931faf0a5
Mathieu PATUREL <australie.p@gmail.com>
2016-12-17 14:19:45 +1100

    Allow to use a simple string to run a command
    instead of list `"commands": [ "save", "close_window"]`

--- chain.py
+++ chain.py
@@ -5,6 +5,10 @@ class ChainCommand(sublime_plugin.WindowCommand):
     def run(self, commands):
         window = self.window
         for command in commands:
-            command_name = command[0]
-            command_args = command[1:]
+            if isinstance(command, str):
+                command_name = command
+                command_args = []
+            else:
+                command_name = command[0]
+                command_args = command[1:]
             window.run_command(command_name, *command_args)
